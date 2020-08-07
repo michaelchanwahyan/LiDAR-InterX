625f42ab5a2ea6a792aadf753ce42f7cdbcd9f28
michael <michaelchan_wahyan@yahoo.com.hk>
2020-08-08 01:27:04 +0800

    initial commit

---
+++ checkHit.m
@@ -0,0 +1,76 @@
+function [isOnSurface2d , gamma_] = checkHit( P1 , P2 , P3, unitVec, startOrigin )
+    % from lidar_origin to u, extrapolate a point to hit W and L
+    % startOrigin + gamma_ * unitVec shall be a point on the line P1P2
+    % gamma_ shall be a nonnegative value
+    % if the gamma_ value is too large,
+    % we return -1 as parallel / nearly parallel wall
+    % are not considered.  isOnSurface2d is also false in this case
+
+    isOnSurface2d = false;
+
+    xo = startOrigin(1); yo = startOrigin(2); zo = startOrigin(3);
+    xu = unitVec(1); yu = unitVec(2); zu = unitVec(3);
+
+    %    a x  +  b y  +  c z  +  d  =  0
+    d = 1;
+    P = [P1; P2; P3]';
+    abc = -P'\ones(3,1);
+    a = abc(1); b = abc(2); c = abc(3);
+    % we use ones(3,1) as the system response vector
+    % thus we enforce d = 1
+    % Assumption taken: origin (0,0,0) is not on the plane
+
+    axo_plus_byo_plus_czo = a * xo + b * yo + c * zo;
+    axu_plus_byu_plus_czu = a * xu + b * yu + c * zu;
+    if (abs(axu_plus_byu_plus_czu) < 1E-3)
+        gamma_ = -1;
+        return;
+    else
+        gamma_ = - ( axo_plus_byo_plus_czo + d ) / axu_plus_byu_plus_czu;
+        if (gamma_ > 1E3)
+            gamma_ = -1;
+            return;
+        end
+    end
+    
+    if (gamma_ < 0)
+        gamma_ = -1;
+        return;
+    end
+
+    q = [ xo ; yo ; zo ] + gamma_ * [ xu ; yu ; zu];
+    %figure; scatter3([P1(1),P2(1),P3(1)], [P1(2),P2(2),P3(2)], [P1(3),P2(3),P3(3)], 'bo'); hold on; scatter3(q(1), q(2), q(3), 'rx');
+    % -------------------------------------------- %
+    % Let f(s) = 0.5 * || P*s - q ||_2^2           %
+    % solve the problem :                          %
+    % \underset{min}{s}    f(s)                    %
+    %     such that                                %
+    %                     s >= 0                   %
+    %                   sum(s) = 1                 %
+    % -------------------------------------------- %
+    % in order to check if point q resides in the simplex formed by P
+    % if q is in P, then objective f(s) should be 0
+    % otherwise, f(s) > 0
+    
+    s   = ones(3,1)/3;
+    PtP = P'*P;
+    L   = max(eig(PtP)); % Lipschitz constant
+    obj_curr = +Inf;
+    for itr = 1 : 1E5
+        GRAD = PtP*s - P'*q;
+        s = SimplexProj((s - GRAD / L)')';
+        Ps_minus_q = P*s - q;
+        obj_prev = obj_curr;
+        obj_curr = Ps_minus_q'*Ps_minus_q;
+        if (obj_curr < 1E-3)
+            break;
+        end
+        if (abs(obj_curr - obj_prev) < 1E-6)
+            break;
+        end
+    end ; clear itr
+
+    if (obj_curr < 1E-3)
+        isOnSurface2d = true;
+    end
+end
---
+++ interx.m
@@ -0,0 +1,196 @@
+close all
+clear
+clc
+
+% inter-cross
+%         |          |
+%      (B)|          |(C)
+% --------.          .--------
+%                    
+%                    
+%                    
+%                    
+%                    
+% --------.          .--------
+%      (A)|          |(D)
+%         |          |
+%
+% (A) := (0,0)
+
+% generate structures
+%     Pa              , Pb              , Pc
+%     xA    yA    zA    xB    yB    zB    xC    yC    zC
+W = [ -100, -100, 0.01, -100,  100, 0.01,  100, -100, 0.01; % ground triangular p1
+      -100,  100, 0.01,  100, -100, 0.01,  100,  100, 0.01; % ground triangular p2
+      -100, -100, -100, -100,  100, -100, -100, -100,  100; % the Left Wall p1
+      -100,  100, -100, -100, -100,  100, -100,  100,  100; % the Left Wall p2
+       100, -100, -100,  100,  100, -100,  100, -100,  100; % the Right Wall p1
+       100,  100, -100,  100, -100,  100,  100,  100,  100; % the Right Wall p2
+      -100, -100, -100, -100, -100,  100,  100, -100, -100; % the Back Wall p1
+      -100, -100,  100,  100, -100, -100,  100, -100,  100; % the Back Wall p2
+      -100,  100, -100, -100,  100,  100,  100,  100, -100; % the Front Wall p1
+      -100,  100,  100,  100,  100, -100,  100,  100,  100; % the Front Wall p2
+      -100, -100,  100, -100,  100,  100,  100, -100,  100; % ground triangular p1
+      -100,  100,  100,  100, -100,  100,  100,  100,  100; % ground triangular p2
+    ];
+
+W = [ W ;
+      20, -60, 0, 20, -20,  0, 20, -60, 20;
+      20, -20, 0, 20, -60, 20, 20, -20, 20;
+      60, -20, 0, 20, -20,  0, 60, -20, 20;
+      20, -20, 0, 60, -20, 20, 20, -20, 20;
+      
+      20,  60, 0, 20,  20,  0, 20,  60, 20;
+      20,  20, 0, 20,  60, 20, 20,  20, 20;
+      60,  20, 0, 20,  20,  0, 60,  20, 20;
+      20,  20, 0, 60,  20, 20, 20,  20, 20;
+      
+      -20, -60, 0, -20, -20,  0, -20, -60, 20;
+      -20, -20, 0, -20, -60, 20, -20, -20, 20;
+      -60, -20, 0, -20, -20,  0, -60, -20, 20;
+      -20, -20, 0, -60, -20, 20, -20, -20, 20;
+      
+      -20,  60, 0, -20,  20,  0, -20,  60, 20;
+      -20,  20, 0, -20,  60, 20, -20,  20, 20;
+      -60,  20, 0, -20,  20,  0, -60,  20, 20;
+      -20,  20, 0, -60,  20, 20, -20,  20, 20;
+    ];
+
+V = [];
+load cardata/private_car_01_data/ENVI.mat % load TRI and Y
+Y(:,2) = Y(:,2) - 10;
+Y(:,1) = Y(:,1) - 7;
+
+%V = [ V ;
+%      [ Y(TRI(:,1),1:3) , Y(TRI(:,2),1:3) , Y(TRI(:,3),1:3) ] ];
+
+
+
+WV = [ W ; V ];
+
+wallNum = size(W,1);
+vehiNum = size(V,1);
+
+% generate road ground
+    roadwidth = 18.5; % meter
+    x = -80:1:80;
+    y = -80:1:80;
+    xx = zeros(numel(x),numel(y));
+    yy = zeros(numel(x),numel(y));
+    zz = zeros(numel(x),numel(y));
+    for i = 1 : numel(x)
+        for j = 1 : numel(y)
+            xx(i,j) = x(i);
+            yy(i,j) = y(j);
+            if ((x(i) >= -roadwidth/2 && x(i) <= roadwidth/2) ...
+             || (y(j) >= -roadwidth/2 && y(j) <= roadwidth/2))
+                zz(i,j) = 0.01;
+            end
+        end
+    end ; clear i j 
+
+% generate LiDAR sensor
+    l_orig = [-roadwidth/2 - 5, -roadwidth/2 - 5, 3.5];
+    l_tilt = [-pi/7, 0, -pi/4];
+    l_high = 0.1;
+    l_diam = 0.1;
+
+    l_surfPt = zeros(3600,3);
+    for i = 1 : 10
+        for j = 1 : 360
+            l_surfPt((i-1)*360+j,1) = l_orig(1) + l_diam * cos(j*pi/180);
+            l_surfPt((i-1)*360+j,2) = l_orig(1) + l_diam * sin(j*pi/180);
+            l_surfPt((i-1)*360+j,3) = l_orig(3) - l_high / 2 + (i-1)*l_high/10;
+        end
+    end ; clear i j
+
+% generate LiDAR points
+l_rps = 5; % rotation per second
+l_bm_count = 64;
+l_bm_azimu = 360;
+%l_bm_azimu = 2048;
+l_bm_angElev = linspace(-pi/8, pi/8, l_bm_count);
+l_bm_angAzim = linspace(0,2*pi,l_bm_azimu+1);
+l_bm_angAzim = l_bm_angAzim(1:l_bm_azimu);
+
+ptNum = l_bm_azimu * l_bm_count;
+PCL = zeros(ptNum, 3);
+
+% construct laser beam unit vector u
+%     x  y  z
+u0 = [0; 1; 0];
+
+ptIdx = 1;
+progress_chk_scale = 1000;
+progress_intvl = floor(ptNum / progress_chk_scale);
+progress_chkpt = 0;
+for azim = l_bm_angAzim
+    if (ptIdx > progress_chkpt)
+        progress_chkpt = progress_chkpt + progress_intvl;
+        t = datetime('now');
+        fprintf('[ %s ] rendering progress = %d / %d\n', t,  ptIdx, ptNum);
+    end
+    for elev = l_bm_angElev
+        c = cos(elev); s = sin(elev);
+        R = [ 1 , 0 , 0 ;   % for elevation turning
+              0 , c ,-s ;   % which is a rotation about the x-axis of u0
+              0 , s , c ];
+        u = R * u0;         % perform elevation turning
+        c = cos(-azim); s = sin(-azim);
+        R = [ c ,-s , 0 ;   % for azimuth turning
+              s , c , 0 ;   % which is a rotation about the z-axis of current u
+              0 , 0 , 1 ];
+        u = R * u;          % perform azimuth turning
+        c = cos(l_tilt(1)); s = sin(l_tilt(1));
+        R = [ 1 , 0 , 0 ;   % for tilting of LiDAR elevation
+              0 , c ,-s ;   % which is a rotation about x axis
+              0 , s , c ];
+        u = R * u;
+        c = cos(l_tilt(3)); s = sin(l_tilt(3));
+        R = [ c ,-s , 0 ;   % for tilting of LiDAR elevation
+              s , c , 0 ;   % which is a rotation about x axis
+              0 , 0 , 1 ];
+        u = R * u;
+        
+        % along the laser beam unit vector, emit a pulse and hit the walls
+        surfCandi = false(1,wallNum + vehiNum); % surface candidates wrt WL
+        gamma_arr = zeros(1,wallNum + vehiNum); % gamma value candidates
+        for wvIdx = 1 : wallNum + vehiNum
+            [ surfCandi(wvIdx) , gamma_arr(wvIdx) ] = checkHit( WV(wvIdx,1:3) , WV(wvIdx,4:6) , WV(wvIdx,7:9), u' , l_orig );
+        end ; clear wIdx
+        gamma_ = min( gamma_arr( surfCandi ) );
+        
+        % save the point
+        PCL(ptIdx,:) = l_orig + gamma_ * u';
+        ptIdx = ptIdx + 1;
+    end
+end ; clear azi ela
+
+PCL = PCL( PCL(:,1) >= -80 & PCL(:,1) <= 80 ...
+    & PCL(:,2) >= -80 & PCL(:,2) <= 80 ...
+    & PCL(:,3) <=30 , : );
+
+% plotting
+figure;
+h1 = surf(xx,yy,zz);
+    set(h1, 'EdgeColor', 'none');
+    axis([-80,80,-80,80,-5,80]); % axis equal
+    hold on;
+
+h2 = plot3(l_surfPt(:,1), l_surfPt(:,2), l_surfPt(:,3), 'k.');
+    set(h2, 'MarkerSize', 0.1);
+    axis equal;
+    hold on;
+
+h3 = plot3(0,0,30,'k.');
+	axis equal;
+    hold on;
+
+h4 = plot3(PCL(:,1), PCL(:,2), PCL(:,3), 'r*');
+    set(h4,'MarkerSize', 0.5);
+    axis equal;
+    hold on;
+
+pc = pointCloud(PCL);
+figure;
+    pcshow(pc);
